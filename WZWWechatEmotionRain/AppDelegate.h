ebd16f1b57de961e48f86f9dcb791dc898ede4f9
wzw110110 <zhiweiwu@zhiweidemacbook-pro.local>
2017-07-25 16:36:16 +0800

    Initial Commit

---
+++ WZWWechatEmotionRain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZWWechatEmotionRain
+//
+//  Created by zhiwei wu on 2017/7/25.
+//  Copyright © 2017年 wzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WZWWechatEmotionRain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WZWWechatEmotionRain
+//
+//  Created by zhiwei wu on 2017/7/25.
+//  Copyright © 2017年 wzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
